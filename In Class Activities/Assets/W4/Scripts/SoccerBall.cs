fee4f46c3d6cfea94b2b1788e2e98563a88cd678
Hajin1221 <130419285+Hajin1221@users.noreply.github.com>
2025-10-20 18:18:33 -0700

    done

--- In Class Activities/Assets/W4/Scripts/SoccerBall.cs
+++ In Class Activities/Assets/W4/Scripts/SoccerBall.cs
@@ -7,6 +7,8 @@ public class SoccerBall : MonoBehaviour
     [SerializeField] private TMP_Text _pointsText;
     [SerializeField] private TMP_Text _timeText;
     [SerializeField] private ParticleSystem _goalVFX;
+    [SerializeField] private int _points;
+    [SerializeField] private float _time;
 
     // STEP 1 -----------------------------------------------------------------
     // The OnTriggerEnter method is a collision method called by Unity that
@@ -21,21 +23,30 @@ public class SoccerBall : MonoBehaviour
     //
     // Also, uncomment and FIX the Debug.Log line.
 
-    //private ?? ??
+    private void OnTriggerEnter(Collider other)
+        
+    {
+        if (other.CompareTag("Goal"))
+        {
+            MadeGoal();
+            
+        }
+            
+    }
     //{
-        // finish STEP 1 by uncommenting and fixing the below line!
-        //Debug.Log(SoccerBall detected a collision with a trigger collider!);
+    // finish STEP 1 by uncommenting and fixing the below line!
+    //
 
-        // STEP 2 -------------------------------------------------------------
-        // Write an IF STATEMENT to check if the game object we collided with
-        //      has the tag "Goal".
-        // Refer to the slides for an example of how to check this :)
-        //
-        // Then, move your Debug.Log() statement so that it's only called if
-        //      the colliding object has the "Goal" tag.
+    // STEP 2 -------------------------------------------------------------
+    // Write an IF STATEMENT to check if the game object we collided with
+    //      has the tag "Goal".
+    // Refer to the slides for an example of how to check this :)
+    //
+    // Then, move your Debug.Log() statement so that it's only called if
+    //      the colliding object has the "Goal" tag.
 
 
-        // STEP 2 -------------------------------------------------------------
+    // STEP 2 -------------------------------------------------------------
     //}
 
     // STEP 1 -----------------------------------------------------------------
@@ -49,17 +60,37 @@ public class SoccerBall : MonoBehaviour
     // Once you've created MadeGoal,
     //      move your Debug.Log() statement into MadeGoal, and
     //      call MadeGoal from inside your if statement in OnTriggerEnter.
-    
-        // STEP 4 -------------------------------------------------------------
-        // _goalVFX is a ParticleSystem, a Component for creating VFX.
-        // ParticleSystem has a method named Play() that displays the VFX:
-        // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
-        //
-        // Call Play on _goalVFX.
 
-        // STEP 4 -------------------------------------------------------------
-    
-    
+    private void MadeGoal()
+    {
+        Debug.Log("SoccerBall detected a collision with a trigger collider!");
+        _goalVFX.Play();
+        
+        _points++;
+        _pointsText.text = "goals: " + _points;
+        _time = 0.0f;
+        
+        _timeText.text = "time: " + _time;
+
+    }
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        _timeText.text = "time: " + _time;
+    }
+
+    // STEP 4 -------------------------------------------------------------
+    // _goalVFX is a ParticleSystem, a Component for creating VFX.
+    // ParticleSystem has a method named Play() that displays the VFX:
+    // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
+    //
+    // Call Play on _goalVFX.
+
+
+
+    // STEP 4 -------------------------------------------------------------
+
+
     // STEP 3 -----------------------------------------------------------------
 
 
@@ -78,7 +109,7 @@ public class SoccerBall : MonoBehaviour
     //      2. Use your MadeGoal method to update the points and text.
     //
     // STEP 5 -----------------------------------------------------------------
-    
+
 
     // STEP 6 -----------------------------------------------------------------
     // Like the last step, these flags do NOT show you where to put all of the
